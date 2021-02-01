82587c8165a83e18b68104e85390705cc8cc4e96
Dieter <dieter.halosta@yahoo.com>
2021-02-01 13:02:03 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>This is a test text</p>
         <a
           className="App-link"
           href="https://reactjs.org"
