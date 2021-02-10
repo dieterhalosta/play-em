549ee3654150dfff9d19c2e9cbcc253407bca271
Dieter <dieter.halosta@yahoo.com>
2021-02-10 12:31:34 +0200

    created popular context

--- src/contexts/popular/popular.context.js
+++ src/contexts/popular/popular.context.js
@@ -1,7 +1,30 @@
-import { createContext } from "react";
+import React, { useState, useEffect } from "react";
 
-import PopularData from "./popular.data";
+import { getPopular } from "../../movieApi/movieApi.utils";
+import Spinner from "../../components/spinner/spinner.component";
+const PopularContext = React.createContext();
 
-const PopularContext = createContext(PopularData);
+export const PopularItemsStore = (props) => {
+  const [popularItems, setPopularItems] = useState([]);
+  const [errorMessage, setErrroMessage] = useState("");
+
+  useEffect(() => {
+    getPopular()
+      .then((data) => setPopularItems(data))
+      .catch((error) => setErrroMessage(error));
+  }, []);
+
+  if (errorMessage) return <div>{errorMessage}</div>;
+
+  if (popularItems.length > 0) {
+    return (
+      <PopularContext.Provider value={popularItems}>
+        {props.children}
+      </PopularContext.Provider>
+    );
+  }
+
+  return <Spinner message='Loading popular items' />;
+};
 
 export default PopularContext;
